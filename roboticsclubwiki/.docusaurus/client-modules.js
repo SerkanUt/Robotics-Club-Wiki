c44506818183d24a1296a6c9184381fe6c0a758a
Nicholasbbb <135785660+Nicholasbbb@users.noreply.github.com>
2024-10-19 21:52:25 -0400

    Delete roboticsclubwiki directory

--- roboticsclubwiki/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\almsi\\roboticsclubwiki\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\almsi\\roboticsclubwiki\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\almsi\\roboticsclubwiki\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\almsi\\roboticsclubwiki\\src\\css\\custom.css"),
-];
