28ff66272c0fdadab12368c9f98a1c956ad84c9e
Nicholasbbb <135785660+Nicholasbbb@users.noreply.github.com>
2024-10-19 21:47:54 -0400

    b

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\almsi\\roboticsclubwiki\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\almsi\\roboticsclubwiki\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\almsi\\roboticsclubwiki\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("C:\\Users\\almsi\\roboticsclubwiki\\src\\css\\custom.css"),
+];
