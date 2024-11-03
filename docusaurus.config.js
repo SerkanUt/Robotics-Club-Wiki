287da08d30eb6fe1934b7a57de7c1d41898cd8aa
Nicholasbbb <135785660+Nicholasbbb@users.noreply.github.com>
2024-11-03 17:03:15 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://nicholasbbb.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/roboticsclubwikitest.github.io/',
+  baseUrl: '/roboticsclubwiki.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
