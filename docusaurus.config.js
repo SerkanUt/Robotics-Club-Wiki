5fb305a904a3a5d8bef205f8df324f0ad13f4208
Nicholasbbb <135785660+Nicholasbbb@users.noreply.github.com>
2024-11-28 20:12:38 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,8 +43,8 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Nicholasbbb/roboticsclubwiki.github.io/',
+          //editUrl:
+            //'https://github.com/Nicholasbbb/roboticsclubwiki.github.io/',
         },
         /**
         blog: {
